891da00eeae10b4a856475342cdadab2fe0c79f9
iancarcha <iancarcha@alu.edu.gva.es>
2023-03-01 23:38:58 +0100

    final final

--- src/main/java/com/example/futbol/model/Arbitros.java
+++ src/main/java/com/example/futbol/model/Arbitros.java
@@ -17,4 +17,36 @@ public class Arbitros {
     private String comite_arb;
 
     private String img_arb;
+
+    public Long getId_arbitro() {
+        return id_arbitro;
+    }
+
+    public void setId_arbitro(Long id_arbitro) {
+        this.id_arbitro = id_arbitro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getComite_arb() {
+        return comite_arb;
+    }
+
+    public void setComite_arb(String comite_arb) {
+        this.comite_arb = comite_arb;
+    }
+
+    public String getImg_arb() {
+        return img_arb;
+    }
+
+    public void setImg_arb(String img_arb) {
+        this.img_arb = img_arb;
+    }
 }
--- src/main/java/com/example/futbol/model/Ligas.java
+++ src/main/java/com/example/futbol/model/Ligas.java
@@ -28,5 +28,69 @@ public class Ligas {
     private Date fin_liga;
 
     private String img_liga;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Equipos getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(Equipos equipos) {
+        this.equipos = equipos;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public Arbitros getArbitros() {
+        return arbitros;
+    }
+
+    public void setArbitros(Arbitros arbitros) {
+        this.arbitros = arbitros;
+    }
+
+    public String getNombre_liga() {
+        return nombre_liga;
+    }
+
+    public void setNombre_liga(String nombre_liga) {
+        this.nombre_liga = nombre_liga;
+    }
+
+    public Date getInicio_liga() {
+        return inicio_liga;
+    }
+
+    public void setInicio_liga(Date inicio_liga) {
+        this.inicio_liga = inicio_liga;
+    }
+
+    public Date getFin_liga() {
+        return fin_liga;
+    }
+
+    public void setFin_liga(Date fin_liga) {
+        this.fin_liga = fin_liga;
+    }
+
+    public String getImg_liga() {
+        return img_liga;
+    }
+
+    public void setImg_liga(String img_liga) {
+        this.img_liga = img_liga;
+    }
 }
 
